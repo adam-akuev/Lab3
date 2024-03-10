0dbfde491cbeef2e0437fb128655b71bf6a943bd
adam-akuev <109418017+adam-akuev@users.noreply.github.com>
2024-03-10 14:06:31 +0300

    Add files via upload

---
+++ Lab3.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    system("chcp 1251");
+    string S;
+    string S0;
+    cout << "Add S: ";
+    getline(cin, S);
+    cout << "Add S0: ";
+    getline(cin, S0);
+    int length = size(S0);
+    int OneElement = S.find(S0);
+    if (S.size() >= S0.size() && OneElement > 0) {
+        S.erase(OneElement, length);
+        cout << "S: " << S;
+    }
+    else
+        cout << "S: " << S;
+}
---
+++ Lab3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    system("chcp 1251");
+    string S;
+    string S0;
+    cout << "Add S: ";
+    getline(cin, S);
+    cout << "Add S0: ";
+    getline(cin, S0);
+    int k = S.find(S0);
+    if (k >= 0)
+        cout << "TRUE";
+    else
+        cout << "ELSE";
+}
